7fba43dea25ceab903af06b7ff8904ba86b7841d
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2023-04-19 18:55:39 -0700

    4/19/23

--- Clase 7/7.1.py
+++ Clase 7/7.1.py
@@ -1,6 +1,7 @@
 import re 
+from data_stark import lista_personajes as lista_heroes 
 
-heroe = { "nombre": "Howard the Duck",
+h = { "nombre": "Howard the Duck",
     "identidad": "Howard (Last name unrevealed)",
     "empresa": "Marvel Comics",
     "altura": "79.349999999999994",
@@ -12,17 +13,18 @@ heroe = { "nombre": "Howard the Duck",
     "inteligencia": ""}
 
 # 1.1
-def extraer_iniciales(nombre_heroe):
+def extraer_iniciales(nombre_heroe:str):
+
+    # recibe como parametro un str omite si existe un 'the' y un '-' y nos devuelve las iniciales separadas con un '.'
     
     if nombre_heroe == "":
         return "N/A"
     else:
         iniciales = ""
         
-        nombre_heroe = nombre_heroe.upper() #me tira error aca cualquiera
+        nombre_heroe = nombre_heroe.upper() 
         
         nombre_heroe = re.sub("THE"," ", nombre_heroe)
-        
         nombre_heroe = re.sub("[-]"," ",nombre_heroe)
         
         lista_nombre_heroe = nombre_heroe.split()
@@ -33,13 +35,53 @@ def extraer_iniciales(nombre_heroe):
         return iniciales
     
 #1.2
-def definir_iniciales_nombre(heroe):
-    iniciales = extraer_iniciales(heroe)
+def definir_iniciales_nombre(heroe:dict):
+
+# recibe como parametro un diciconario, valida quetrnga el formato correcto, si la clave iniciales no existe la agrega y devuelve true
+
+
     if type(heroe) != dict or "nombre" not in heroe:
+
         return False
+    
     else:
         if "iniciales" not in heroe:
-            heroe["iniciales"] = iniciales
-            return True
+            heroe["iniciales"] = extraer_iniciales(heroe['nombre'])
 
-definir_iniciales_nombre(heroe)
+            return True
+        
+#1.3
+def agregar_iniciales_nombre(lista:list):
+
+# itera y le agrega a cada heroe las iniciales  
+
+    if type(lista) == list and len(lista) > 0:
+            
+        for heroe in lista:  
+
+            validacion = definir_iniciales_nombre(heroe) 
+            print(heroe)
+            if validacion == False:
+
+                print('El origen de los dato con contiene el formato correcto')
+
+        return True
+    
+    else:
+
+        return False
+
+#1.4
+def stark_imprimir_nombres_con_iniciales(lista_heroes): 
+
+    agregar_iniciales_nombre(lista_heroes)
+    print(i['nombre'])
+
+
+
+
+
+
+agregar_iniciales_nombre(lista_heroes) 
+
+                
