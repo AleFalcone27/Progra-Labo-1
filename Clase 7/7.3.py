df46d08613e800575271feb039649150c653408e
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2023-04-21 14:46:01 -0300

    21/4/23

--- Clase 7/7.3.py
+++ Clase 7/7.3.py
@@ -1,27 +1,56 @@
 from data_stark import lista_personajes
 import re
 
-numero_str = "23"
-
+num_str = ""
 
 def sanitizar_entero(num_str:str):
     
     num_str = num_str.strip()
     
-    if num_str.isnumeric() == False:
-            return -1
+    if re.search("[a-zA-Z]+",num_str):
+        return -1
+    elif re.search("^-", num_str):
+        return -2
+    elif re.search("[0-9+]",num_str):
+        num_str = int(num_str)
+        return num_str
     else:
-        return True
+        return -3
+
+def sanitizar_flotante(num_str:str):
+    
+    num_str = num_str.strip()
+    
+    if re.search("[a-zA-Z]+", num_str):
+        return -1
+    elif re.search("^-",num_str):
+        return -2
+    elif re.search("[0-9+]",num_str):
+        num_str = float(num_str)
+        return num_str
+    else: 
+        return -3
+
+def sanitizar_string(num_str:str, valor_por_defecto="-"):
+    
+    num_str = re.sub("/"," ",num_str)
+    
+    if re.search("[0-9+]",num_str):
+        return "N/A"
+    elif num_str == "":
+        valor_por_defecto = valor_por_defecto.lower()
+        return valor_por_defecto
+    else:
+        num_str = num_str.lower()
+        return num_str
+
+       
 
     
-    # print(re.search("[0-9]",numero_str))
-    #contiene caracteres no numericos return -1
+
     
-    # elif #el numero es negativo return -2
-    
-    # elif:
-    
-    # else:
-    #     return -3
-    
-print(sanitizar_entero(numero_str))
+
+
+# print(sanitizar_flotante(num_str))
+# print(sanitizar_entero(num_str))
+print(sanitizar_string(num_str))
