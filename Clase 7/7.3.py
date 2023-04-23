57a2a39ec090098652be0f877f317a57f7c49782
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2023-04-23 14:10:22 -0300

    23/4/23

--- Clase 7/7.3.py
+++ Clase 7/7.3.py
@@ -15,6 +15,8 @@ h = { "nombre": "Howard the Duck",
 # 3.1
 def sanitizar_entero(num_str:str):
     
+# esta funcion determina si el str recibido como parametro es un numero entero positivo y lo retorna  
+       
     num_str = num_str.strip()
     
     if re.search("[a-zA-Z]+",num_str):
@@ -30,6 +32,8 @@ def sanitizar_entero(num_str:str):
 # 3.2
 def sanitizar_flotante(num_str:str):
     
+# esta funcion determina si el str recibido como parametro es un flotante positivo
+  
     num_str = num_str.strip()
     
     if re.search("[a-zA-Z]+", num_str):
@@ -44,7 +48,9 @@ def sanitizar_flotante(num_str:str):
 
 # 3.3
 def sanitizar_string(num_str:str, valor_por_defecto="-"):
-    
+
+# determina si el str recibido como parametro es solo texto
+
     num_str = re.sub("/"," ",num_str)
     
     if re.search("[0-9+]",num_str):
@@ -59,6 +65,7 @@ def sanitizar_string(num_str:str, valor_por_defecto="-"):
 # 3.4
 def sanitizar_dato(heroe:dict,clave:str,tipo_dato:str):
 
+# sanitiza utilizando las funciones creadas en los puntos anteriores segun el tipo de dato que le pasemos como parametro 
 # heroe: diccionario con los datos de un personaje
 # clave: clave del valor que queremos sanitizar en el diccionario
 # tipo_dato: str que representa el tipo de dato a sanitizar
@@ -75,23 +82,29 @@ def sanitizar_dato(heroe:dict,clave:str,tipo_dato:str):
         dato_sanitizado = sanitizar_entero(clave) 
         retorno = dato_sanitizado
 
-    elif tipo_dato != 'str' or tipo_dato != 'float' or tipo_dato != 'str':
-        print('Tipo de dato no reconocido')
+    # if tipo_dato != 'str' or tipo_dato != 'float' or tipo_dato != 'int':
+    #     print('Tipo de dato no reconocido')
 
-    elif clave not in heroe:
-         print("La clave especificada no existe en el heroe")
+    # if clave not in heroe:
+    #     print("La clave especificada no existe en el heroe")
 
     return retorno
 
+# 3.5
 def stark_normalizar_datos(lista_personajes:list[dict]):
-    
+
+# recorre la lista pasada como argumento y santiza cada uno de los valores de las claves que especifiquemos
+
     for heroe in lista_personajes:
         heroe['altura'] = sanitizar_dato(heroe,heroe["altura"],'float')
         heroe['peso'] = sanitizar_dato(heroe,heroe["peso"],'float')
         heroe['color_ojos'] = sanitizar_dato(heroe,heroe["color_ojos"],'str')
         heroe['color_pelo'] = sanitizar_dato(heroe,heroe["color_pelo"],'str')
-        heroe['fuerza'] = sanitizar_dato(heroe,heroe["fuerza"],'str')
+        heroe['fuerza'] = sanitizar_dato(heroe,heroe["fuerza"],'int')
         heroe['inteligencia'] = sanitizar_dato(heroe,heroe["inteligencia"],'str')
-        print(heroe)
+        if len(lista_personajes) > 0:
+            print("Datos Normalizados")
+        else:
+            print("Error: Lista de héroes vacía")
         
 stark_normalizar_datos(lista_personajes)
---
+++ Clase 7/7.4.py
@@ -0,0 +1,35 @@
+from data_stark import lista_personajes
+import re
+
+# 4.1
+def generar_indices_nombres(lista:list[dict]):
+
+# recibimos como parametro una lista de diccionarios
+# iteramos la lista de personajes y por cada nombre que nos encontramos lo separamos y generamos una nueva lista   
+
+        lista_nombres = []
+        lista_nombres_separados =[]
+        for heroe in lista:
+            if len(lista) > 0 and type(heroe) == dict and "nombre" in heroe:
+                lista_nombres.append(heroe["nombre"])
+            else:               
+                print("El origen de los datos no contiene un formato")
+        for nombre in lista_nombres:
+                lista_nombres_separados.extend(nombre.split()) 
+                # con split generamos de cada cadena subcadenas separadas por coma
+                # con extend agregamos cada uno de los elementos no como listas sino como elementos individuales (sin corchetes) esto no pasa si utilizamos append.
+        return lista_nombres_separados
+
+# 4.2
+def stark_imprimir_indice_nombre(lista:list[dict]):
+    
+# unimos los nombres separados en la funcion 4.1 con "-"
+
+    lista_nombres = generar_indices_nombres(lista_personajes)
+    lista_nombres = "-".join(lista_nombres)
+    print(lista_nombres)
+
+stark_imprimir_indice_nombre(lista_personajes)
+
+
+
